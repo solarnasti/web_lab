a6dd4a1622a2565707b8d1bbd8398f2128b6697d
Anastasia Goncharova <anasteisha.i.g@mail.ru>
2020-11-30 11:55:29 +0300

    react-test lab_ с хуками

--- react-test/webpack.config.js
+++ react-test/webpack.config.js
@@ -2,16 +2,26 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    entry:"./src/index.js",
+    mode: 'development',
+    entry: "./src/index.jsx",
     output:{
         path:path.join(__dirname, "/dist"),
         filename:"index_bundle.js"
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
     },
     module:{
         rules: [
             {
                 test:/\.js$/,
                 exclude: /node_modules/,
+                use:{
+                    loader:"babel-loader"
+                }
+            },
+            {
+                test:/\.jsx$/,
                 use:{
                     loader:"babel-loader"
                 }
@@ -25,12 +35,12 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: __dirname + "/public/index.html",
-            inject: 'body'
         })
     ],
     devServer: {
-        publicPath: "/",
-        contentBase: "./public",
-        hot: true
+        port: 3000,
+        open: true,
+        historyApiFallback: true,
     },
+    devtool: 'source-map',
 }
